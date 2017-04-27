0a6e4a156a51704e497d9335cbd9f11b029e1cb2
William Petit <wpetit@cadoles.com>
2017-04-27 10:20:52 +0200

    Nettoyage complet des cookies en fin de déconnexion

--- examples/clients-example.js
+++ examples/clients-example.js
@@ -6,4 +6,10 @@ module.exports = [
     client_secret: 'AbsolutlyNotSecret',
     post_logout_redirect_uris: [ 'http://localhost:3333/debug/logout/cb' ],
   },
+  {
+    client_id: 'e-users',
+    redirect_uris: [ 'http://localhost:3000/auth/openid/callback' ],
+    client_secret: 'NotSoSecret',
+    post_logout_redirect_uris: [ 'http://localhost:3000/' ],
+  },
 ];
